c582c4d32f3cb17f26b867d140dbc7f605cc67ed
maorre <67946243+maorre@users.noreply.github.com>
2023-01-09 14:35:09 +0200

    Commit message

---
+++ 3.3.js
@@ -0,0 +1,30 @@
+//q1
+function percentageOfWorld1(population) { return population/(7990*1000000)*100 };
+
+let retValChina, retValIsrael, retValFrance;
+
+retValChina = percentageOfWorld1(1441000000);
+retValIsrael = percentageOfWorld1(9300000);
+retValFrance =  percentageOfWorld1(68000000);
+
+console.log(`In the China has ${retValChina}% of the world population`)
+console.log(`In the Israel has ${retValIsrael}% of the world population`)
+console.log(`In the France has ${retValFrance}% of the world population`)
+
+//q2
+
+
+const  percentageOfWorld2 = (population)=> population/(7990*1000000)*100 ;
+
+let retValChina2, retValIsrael2, retValFrance2;
+
+retValChina2 = percentageOfWorld2(1441000000);
+retValIsrael2 = percentageOfWorld2(9300000);
+retValFrance2 =  percentageOfWorld2(68000000);
+
+
+console.log(`In the China has ${retValChina2}% of the world population`)
+console.log(`In the Israel has ${retValIsrael2}% of the world population`)
+console.log(`In the France has ${retValFrance2}% of the world population`)
+
+
